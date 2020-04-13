7dd95f7f8b3511032f8a3886a8635d24e28e2e4e
Ason <aloha_sonza@hotmail.com>
2020-04-13 19:07:42 +0700

    good

--- post/form.py
+++ post/form.py
@@ -1,20 +1,16 @@
 from django import forms
 from django.forms import ModelForm, widgets
 from django.forms.widgets import Textarea
-
 from post.models import Post
 
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['text', 'tag']
+        fields = ['text']
         widgets = {
             'text': forms.Textarea(attrs= {
                 'class': 'form-control card-text',
                 'rows': '3'
-            }),
-            'tag': forms.Select(attrs= {
-                'class': 'form-control'
             })
         }
--- post/models.py
+++ post/models.py
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
-=======
-from idlelib.textview import view_file
-
->>>>>>> parent of 9b48b3e... view all post
 from django.db import models
-from django.contrib.auth.models import User
-
-
-tag_choices = [
-        ('POL', 'Politics'),
-        ('GEN', 'General'),
-]
+from user.models import RandomUserModel
 
 class Post(models.Model):
     view = models.IntegerField(default=0)
@@ -21,7 +10,7 @@ class Post(models.Model):
     sad = models.IntegerField(default=0)
     angry = models.IntegerField(default=0)
     numberOfDistribution = models.IntegerField(default=0)
-    createBy = models.ForeignKey(User, on_delete=models.CASCADE)
+    createBy = models.ForeignKey(RandomUserModel, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['time']
