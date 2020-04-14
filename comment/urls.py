30efc4e461a4f77f0bcca4abd0dc6a2c19e93218
toplordsaito2 <61070214@kmitl.ac.th>
2020-04-14 20:14:24 +0700

    Emotion + Realtime

--- comment/urls.py
+++ comment/urls.py
@@ -5,4 +5,5 @@ from comment import views
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
     path('addComment/<int:pk>', views.addComment, name='addComment'),
+    path('emotion/<int:pk>/<int:type_emotion>', views.emotionComment, name='emotion_comment')
 ]
--- comment/views.py
+++ comment/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from post.models import Post
 from comment.models import Comment
 from comment.forms import CommentForm
+from django.views.decorators.csrf import csrf_exempt
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+from django.http import HttpResponse
+
 
 def addComment(request, pk):
     post = Post.objects.get(pk=pk)
@@ -15,3 +20,31 @@ def addComment(request, pk):
         comment.save()
         print('Comment Success')
         return redirect('view_all_posts')
+
+
+@csrf_exempt
+def emotionComment(request, pk, type_emotion):
+    comment = Comment.objects.get(pk=pk)
+    if type_emotion == 1:
+        comment.like += 1
+        message(comment.post_id.id, {"like": comment.like, "comment":comment.id})
+    elif type_emotion == 2:
+        comment.haha += 1
+        message(comment.post_id.id, {"haha": comment.haha, "comment":comment.id})
+    elif type_emotion == 3:
+        comment.angry += 1
+        message(comment.post_id.id, {"angry": comment.angry, "comment":comment.id})
+    elif type_emotion == 4:
+        comment.sad += 1
+        message(comment.post_id.id, {"sad": comment.sad, "comment":comment.id})
+    comment.save()
+    return HttpResponse(status=201)
+
+
+def message(post_id, json):
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        "post_post%d" % post_id, {
+            'type': 'chat_message',
+            'message': json
+        })
