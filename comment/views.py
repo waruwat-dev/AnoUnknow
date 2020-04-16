a86605d0b949f903ccfb9ee5122aeefac05d778e
toplordsaito2 <61070214@kmitl.ac.th>
2020-04-16 16:39:52 +0700

    comment_API

--- comment/views.py
+++ comment/views.py
@@ -5,21 +5,27 @@ from comment.forms import CommentForm
 from django.views.decorators.csrf import csrf_exempt
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .serializers import CommentCreateSerializer, CommentSerializer
+from rest_framework.decorators import api_view
 
 
+# @api_view(['POST'])
+@csrf_exempt
 def addComment(request, pk):
-    post = Post.objects.get(pk=pk)
-    print(post)
-    form = CommentForm(request.POST)
-    if form.is_valid():
-        comment = Comment.objects.create(
-            text=form.cleaned_data['text'],
-            post_id=post
-        )
-        comment.save()
-        print('Comment Success')
-        return redirect('view_all_posts')
+    if request.method == 'POST':
+        post = Post.objects.get(pk=pk)
+        serializer = CommentCreateSerializer(data=request.POST)
+        if serializer.is_valid():
+            comment = serializer.save(post_id=post)
+            message(pk, {"addComment": comment.id})
+            return HttpResponse(status=201)
+    return HttpResponse(status=400)
+
+def getComment(request, pk):
+    comment = Comment.objects.get(pk=pk)
+    serializer = CommentSerializer(comment)
+    return JsonResponse(serializer.data, status=200)
 
 
 @csrf_exempt
@@ -28,7 +34,8 @@ def emotionComment(request, pk, type_emotion):
     if type_emotion == 1:
         comment.like += 1
         message(comment.post_id.id, {
-                "like": comment.like, "comment": comment.id})
+                "like": comment.like,
+                "comment": comment.id})
     elif type_emotion == 2:
         comment.haha += 1
         message(comment.post_id.id, {
@@ -46,6 +53,7 @@ def emotionComment(request, pk, type_emotion):
 
 
 def message(post_id, json):
+
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(
         "post_post%d" % post_id, {
--- static/js/post.js
+++ static/js/post.js
@@ -32,32 +32,84 @@ async function emotion_comment(comment, type, element) {
 //         .then(response => console.log('Success:', JSON.stringify(response)))
 //         .catch(error => console.error('Error:', error));
 // }
-function postCreate(e, url) {
-    var text = e.getElementsByClassName("text")
-    // var url = "{% url 'create_post' %}"
+// function postCreate(e, url) {
+//     var text = e.getElementsByClassName("text")
+//     // var url = "{% url 'create_post' %}"
+//     fetch(url, {
+//         method: 'POST',
+//         body: {
+//             text
+//         }
+//     })
+//         // .then(res => console.log(res))
+//         .catch(err => alert(err))
+//     console.log(url)
+
+// }
+
+function commentCreate(post_id, url) {
+    var comment = document.getElementById("comment_post" + post_id)
+    var text = comment.value
+    comment.value = ""
+    const formData = new FormData();
+    formData.append('text', text);
     fetch(url, {
         method: 'POST',
-        body: {
-            text
-        }
+        body: formData
     })
         // .then(res => console.log(res))
         .catch(err => alert(err))
-    console.log(url)
+}
 
+function getComment(comment_id) {
+    fetch('/comment/getcomment/' + comment_id)
+        .then(res => res.json())
+        .then(data => insertComment(data))
+        .catch(err => alert(err))
+
+}
+
+function insertComment(comment) {
+    var div = document.createElement('div');
+    var post = document.getElementById("post_" + comment.post_id)
+    var commentArea = post.getElementsByClassName('comment_area')[0]
+    var html = `
+    <div class="card m-1" id="comment_${comment.id}">
+        <div class="card-body">
+            <h6>${comment.text}</h6>
+            <div class="btn-group mr-2" role="group" aria-label="First group">
+                <button type="submit" class="btn btn-secondary"
+                    onclick="emotion_comment(${comment.id}, 1, this)">👍<span
+                        class="like">${comment.like}</span></button>
+                <button type="submit" class="btn btn-secondary"
+                    onclick="emotion_comment(${comment.id}, 2, this)">😂<span
+                        class="haha">${comment.haha}</span></button>
+                <button type="submit" class="btn btn-secondary"
+                    onclick="emotion_comment(${comment.id}, 3, this)">😠<span
+                        class="angry">${comment.angry}</span></button>
+                <button type="submit" class="btn btn-secondary"
+                    onclick="emotion_comment(${comment.id}, 4, this)">😭<span
+                        class="sad">${comment.sad}</span></button>
+            </div>
+        </div>
+    </div>`
+    div.innerHTML = html
+    commentArea.appendChild(div)
 }
 
 
 function socketOnMessage(e) {
     var data = JSON.parse(e.data).message;
     // console.log(data)
-
+    if (data.addComment) {
+        getComment(data.addComment)
+        return
+    }
     if (data.comment) {
         var element = document.getElementById("comment_" + data.comment)
     } else {
         var element = document.getElementById("post_" + data.post)
     }
-    console.log(element)
     if (data.like) {
         var like = element.getElementsByClassName("like")[0]
         like.innerText = data.like
