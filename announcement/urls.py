2067709c5075c20fb7afafe44f08badf5a76af87
Ason <aloha_sonza@hotmail.com>
2020-04-17 18:55:35 +0700

    Manage is allowed
    -Add
    -Delete
    -Edit
    but not good

--- announcement/urls.py
+++ announcement/urls.py
@@ -5,7 +5,6 @@ from announcement import views
 urlpatterns = [
     path('view_announcement/', views.viewAnnounce, name='view_announcement'),
     path('add_announcement/', views.announce, name='announce'),
-    path('edit_announcement/', views.editAnnounce, name='edit_announcement'),
-    path('delete_announcement/', views.deleteAnnounce, name='delete_announcement'),
-    
+    path('edit_announcement/<int:announcement_id>', views.editAnnounce, name='edit_announcement'),
+    path('delete_announcement/<int:announcement_id>', views.deleteAnnounce, name='delete_announcement')
 ]
--- announcement/views.py
+++ announcement/views.py
@@ -1,30 +1,38 @@
-from django.shortcuts import render
-from .form import AnnouncementForm
-from .models import AnnouncementModel
-from user.models import Authen_User
 import datetime
+from django.shortcuts import redirect, render
+from user.models import Authen_User
+from .form import AnnouncementForm
+from .models import AnnouncementModel, typeOfAnnouncement
+
 
 def viewAnnounce(request):
-    pass
+    announcements = AnnouncementModel.objects.filter(is_active=True)
+    typeDict = {t[0]: t[1] for t in typeOfAnnouncement}
+    for announcement in announcements:
+        announcement.type = typeDict[announcement.type]
+    print(announcements)
+    context = {
+        'announcements': announcements
+    }
+
+    return render(request, 'announcement/view_announcement.html', context=context)
+
 
 def announce(request):
     now = datetime.datetime.now()
     admin = request.user.authen_user.getAdmin()
+
     if request.method == 'POST':
         form = AnnouncementForm(request.POST)
         if form.is_valid():
             start_time = form.cleaned_data['start_time']
-            is_active=False
+            form = form.save()
             if start_time > now:
-                is_active=True
-            AnnouncementModel.objects.create(
-                text=form.cleaned_data['text'],
-                start_time=form.cleaned_data['start_time'],
-                end_time=form.cleaned_data['end_time'],
-                is_active=is_active,
-                type=form.cleaned_data['type'],
-                adminId=admin
-            )
+                form.is_active = True
+            form.adminId = admin
+            form.save()
+            print('Create Announcement --', admin.user.username)
+            return redirect('view_announcement')
     else:
         form = AnnouncementForm()
     
@@ -36,8 +44,36 @@ def announce(request):
     return render(request, 'announcement/add_announcement.html', context=context)
 
 
-def editAnnounce(request):
-    pass
+def editAnnounce(request, announcement_id):
+    now = datetime.datetime.now()
+    admin = request.user.authen_user.getAdmin()
+    announcement = AnnouncementModel.objects.get(pk=announcement_id)
 
-def deleteAnnounce(request):
-    pass
+    if request.method == 'POST':
+        form = AnnouncementForm(request.POST)
+        if form.is_valid():
+            start_time = form.cleaned_data['start_time']
+            form = form.save()
+            if start_time > now:
+                form.is_active = True
+            form.adminId = admin
+            form.save()
+            print('Create Announcement --', admin.user.username)
+            return redirect('view_announcement')
+    else:
+        form = AnnouncementForm(instance=announcement)
+    
+    context = {
+        'form': form,
+        'title': 'Edit Anouncement'
+    }
+
+    return render(request, 'announcement/add_announcement.html', context=context)
+    
+
+def deleteAnnounce(request, announcement_id):
+    announcement = AnnouncementModel.objects.get(pk=announcement_id)
+    announcement.is_active = False
+    announcement.save()
+    print('Deleted --', announcement.id)
+    return redirect('view_announcement')
